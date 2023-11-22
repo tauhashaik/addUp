2ab773bafb93300041f396d4e5fad3abadc30713
Tauha Shaik <tauhashaik@Tauhas-MacBook-Air.local>
2023-11-22 20:41:49 +0200

    calculation completed

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,20 +1,18 @@
     // Create Array
     let arNum = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
-    let n = 0
+    let n = 0;
             // Creating Variables for button and display
-            let nextInput = document.getElementById('nextInput');
+            let nextInput = document.getElementById('addInput');
             let calcbtn = document.getElementById('calcbtn');
     
         // addEventListener for button functionality
-            calcbtn.addEventListener('click', displayNextNumber)
+            calcbtn.addEventListener('click', add)
     
             // function created to add all values inside array into one total.
             function add(){
-                if(n < arNum.length){
-                    let next = arNum[n];
-                    // console.log()
-                    nextInput.value = next
-                }else{
-                    n = 0
-                } 
+                let sum = 0;
+                for (n = 0; n < arNum.length; n++) {
+                    sum += arNum[n];
+                }
+                addInput.value = sum;
             }
